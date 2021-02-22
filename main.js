c08cf99090c2f89e9f497863abf24f5968f2ccfc
mamii <51394140+mamii07180@users.noreply.github.com>
2021-02-22 22:25:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 (function(){
     // 設定
     var testTimer;
-    var interval =10000; // 切り替わりの間隔（ミリ秒）
-    var fade_speed = 500;// フェード処理の早さ（ミリ秒）
+    var interval =1000; // 切り替わりの間隔（ミリ秒）
+    var fade_speed = 50;// フェード処理の早さ（ミリ秒）
 
     $("#img").hide();
     $(".fade-img-box img:first").addClass("active").show();
